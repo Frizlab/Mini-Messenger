cc65a5e55f16ffe56b4dab360d546ec4e0a672de
Frizlab <fload@me.com>
2017-07-13 15:25:09 +0200

    Add error message with retry button on load error

--- Mini Messenger/ViewController.swift
+++ Mini Messenger/ViewController.swift
@@ -12,13 +12,16 @@ import UIKit
 
 class ViewController: UIViewController, UIWebViewDelegate {
 	
-	@IBOutlet var loadingView: UIView!
+	@IBOutlet var viewLoading: UIView!
+	@IBOutlet var viewLoadingError: UIView!
+	@IBOutlet var labelErrorDescription: UILabel!
+	
 	@IBOutlet var webView: UIWebView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		webView.loadRequest(URLRequest(url: URL(string: "https://facebook.com/messages/")!))
+		loadMainPage()
 	}
 	
 	override func didReceiveMemoryWarning() {
@@ -30,12 +33,40 @@ class ViewController: UIViewController, UIWebViewDelegate {
 		webView.goBack()
 	}
 	
+	@IBAction func reload(_ sender: AnyObject) {
+		loadMainPage()
+	}
+	
 	func webViewDidStartLoad(_ webView: UIWebView) {
-		UIView.animate(withDuration: 0.25){ self.loadingView.alpha = 1 }
+		UIView.animate(withDuration: 0.25){ self.viewLoading.alpha = 1; self.viewLoadingError.alpha = 0; self.webView.alpha = 1 }
 	}
 	
 	func webViewDidFinishLoad(_ webView: UIWebView) {
-		UIView.animate(withDuration: 0.25){ self.loadingView.alpha = 0 }
+		UIView.animate(withDuration: 0.25){ self.viewLoading.alpha = 0; self.viewLoadingError.alpha = 0; self.webView.alpha = 1 }
+	}
+	
+	func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+		labelErrorDescription.text = error.localizedDescription
+		UIView.animate(withDuration: 0.25){ self.viewLoading.alpha = 0; self.viewLoadingError.alpha = 1; self.webView.alpha = 0 }
+	}
+	
+	private func loadMainPage() {
+		webView.loadRequest(URLRequest(url: URL(string: "https://facebook.com/messages/")!))
+	}
+	
+	private func imageWithColor(_ color: UIColor) -> UIImage {
+		let rect = CGRect(x: 0, y: 0, width: 1 , height: 1)
+		
+		UIGraphicsBeginImageContext(rect.size)
+		let context = UIGraphicsGetCurrentContext()
+		
+		context?.setFillColor(color.cgColor)
+		context?.fill(rect)
+		
+		let image = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+		
+		return image!
 	}
 	
 }
