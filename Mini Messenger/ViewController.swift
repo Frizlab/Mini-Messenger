7e6592c21c63e9814af9c6673166c00a2e9e04a2
Frizlab <fload@me.com>
2017-07-13 14:14:09 +0200

    Initial Commit

---
+++ Mini Messenger/ViewController.swift
@@ -0,0 +1,23 @@
+/*
+ * ViewController.swift
+ * Mini Messenger
+ *
+ * Created by François Lamboley on 13/07/2017.
+ * Copyright © 2017 François Lamboley. All rights reserved.
+ */
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+	
+}
