9732ba817de9f1370d1f09b1f4a0ec82720df806
Frizlab <fload@me.com>
2017-07-13 14:38:20 +0200

    Implement App

--- Mini Messenger/ViewController.swift
+++ Mini Messenger/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
+	
+	@IBOutlet var loadingView: UIView!
+	@IBOutlet var webView: UIWebView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		webView.loadRequest(URLRequest(url: URL(string: "https://facebook.com/messages/")!))
 	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
+	}
+	
+	func webViewDidStartLoad(_ webView: UIWebView) {
+		UIView.animate(withDuration: 0.25){ self.loadingView.alpha = 1 }
+	}
+	
+	func webViewDidFinishLoad(_ webView: UIWebView) {
+		UIView.animate(withDuration: 0.25){ self.loadingView.alpha = 0 }
 	}
 	
 }
